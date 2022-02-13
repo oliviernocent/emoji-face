0987a39c2626dca487f2c063d058304d412ee8bf
oliviernocent <olivier.nocent@univ-reims.fr>
2022-02-13 18:59:17 +0100

    Released!

---
+++ README.md
@@ -0,0 +1,7 @@
+# Emoji Face
+
+A web application based on [p5.js](https://p5js.org/) + [ml5.js](https://ml5js.org/) that displays an emoji on top of your head, captured by your webcam.
+
+![screenshot #1](img/screenshot.jpg)
+
+Try the live [demo](https://oliviernocent.github.io/emoji-face/).
---
+++ js/sketch.js
@@ -0,0 +1,145 @@
+let mode = "realism";
+let selectMode;
+
+let shuffleButton;
+
+let emoji = [];
+let mask = [];
+
+let poseNet, poses = [];
+/*
+Keypoint label
+
+ 0 nose
+ 1 leftEye
+ 2 rightEye
+ 3 leftEar
+ 4 rightEar
+ 5 leftShoulder
+ 6 rightShoulder
+ 7 leftElbow
+ 8 rightElbow
+ 9 leftWrist
+10 rightWrist
+11 leftHip
+12 rightHip
+13 leftKnee
+14 rightKnee
+15 leftAnkle
+16 rightAnkle
+
+*/
+
+function preload() {
+  let filename = [
+    "octopus.png",
+    "alarm-clock.png",
+    "alien-monster.png",
+    "basketball.png",
+    "bear-face.png",
+    "billiards.png",
+    "bomb.png",
+    "cat-face.png",
+    "clown-face.png",
+    "cow-face.png",
+    "dog-face.png",
+    "dragon-face.png",
+    "extraterrestrial-alien.png",
+    "fox-face.png",
+    "frog-face.png",
+    "game-die.png",
+    "hamster-face.png",
+    "koala-face.png",
+    "lion-face.png",
+    "monkey-face.png",
+    "mouse-face.png",
+    "musical-keyboard.png",
+    "package.png",
+    "panda-face.png",
+    "pig-face.png",
+    "pile-of-poo.png",
+    "rabbit-face.png",
+    "robot-face.png",
+    "skull.png",
+    "television.png",
+    "tiger-face.png"
+  ];
+
+  for (let file of filename) {
+    let img1 = loadImage("img/emoji/flat/" + file);
+    let img2 = loadImage("img/emoji/realism/" + file);
+
+    emoji.push({ flat: img1, realism: img2 });
+  }
+}
+
+function setup() {
+  selectMode = select("#mode");
+  selectMode.changed(changeMode);
+
+  shuffleButton = select("#shuffle");
+  shuffleButton.mousePressed(shuffleMasks);
+
+  let canvas = createCanvas(640, 480);
+  canvas.parent("canvas");
+
+  capture = createCapture(VIDEO);
+  capture.size(640, 480);
+  capture.hide();
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(capture, "multiple", modelReady);
+
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function (results) {
+    poses = results;
+
+    for (let i=0; i<poses.length; i++) {
+      if (mask[i] === undefined) {
+        mask[i] = Math.floor(Math.random() * emoji.length);
+      }
+    }
+  });
+
+  colorMode(RGB, 255, 255, 255, 1);
+}
+
+function draw() {
+  imageMode(CORNER);
+  image(capture, 0, 0, 640, 480);
+
+  for (let i=0; i<poses.length; i++) {
+    let p1 = poses[i].pose.keypoints[2].position;
+    let p2 = poses[i].pose.keypoints[1].position;
+
+    let v = createVector(p2.x - p1.x, p2.y - p1.y);
+    let l = v.mag();
+    let angle = v.heading();
+
+    push()
+    translate((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+    rotate(angle);
+    imageMode(CENTER);
+    image(emoji[mask[i]][mode], 0, 0, l * 5, l * 5);
+    pop();
+  }
+}
+
+function modelReady() {
+  console.log("Model ready...");
+}
+
+function changeMode() {
+  mode = selectMode.value();
+}
+
+function shuffleMasks() {
+  mask = [];
+  for (let i=0; i<poses.length; i++) {
+    if (mask[i] === undefined) {
+      mask[i] = Math.floor(Math.random() * emoji.length);
+    }
+  }
+
+}
